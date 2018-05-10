1e4ad90579abd1a6a609e4882467ab91186620da
Susanne Kaiser <su.kaiser@gmail.com>
2018-05-11 00:08:31 +0200

    gdpr-adjustments: deactivated fb pixel

--- frontend/components/registration/index.jsx
+++ frontend/components/registration/index.jsx
@@ -28,31 +28,6 @@ class Registration extends Component {
     return createUser(formValues);
   }
 
-  componentDidMount () {
-    const script = document.createElement("script");
-    script.type = 'text/javascript';
-    script.text = "!function(f,b,e,v,n,t,s)" +
-      "{if(f.fbq)return;n=f.fbq=function(){n.callMethod?" +
-      "n.callMethod.apply(n,arguments):n.queue.push(arguments)};" +
-      "if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';" +
-      "n.queue=[];t=b.createElement(e);t.async=!0;" +
-      "t.src=v;s=b.getElementsByTagName(e)[0];" +
-      "s.parentNode.insertBefore(t,s)}(window, document,'script'," +
-      "'https://connect.facebook.net/en_US/fbevents.js');" +
-      "fbq('init', '1774176022832775');" +
-      "fbq('track', 'PageView');";
-    document.body.appendChild(script);
-
-    const noscript = document.createElement("noscript");
-    const pixelImg = document.createElement("img");
-    pixelImg.height = 1;
-    pixelImg.width = 1;
-    pixelImg.style = 'display:none';
-    pixelImg.src = 'https://www.facebook.com/tr?id=1774176022832775&ev=PageView&noscript=1';
-    noscript.appendChild(pixelImg);
-    document.body.appendChild(noscript);
-  }
-
   registrationForm() {
     const { t } = this.context;
     const { selectedRole, submitting } = this.props;
