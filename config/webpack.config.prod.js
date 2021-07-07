e91a45945a8f8cda4f0519eaa377a0d6554d6a64
Andree Wille <AndreeWille@users.noreply.github.com>
2021-07-07 10:31:44 +0200

    build: replace ExtractTextWebpackPlugin with MiniCssExtractPlugin
    since webpack v4 the extract-text-webpack-plugin should not be used for css. Use mini-css-extract-plugin instead.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,23 +1,23 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpackConfig = require('./webpack.config.base').config;
 
-const extractCss = new ExtractTextPlugin('styles-[contenthash].css');
 
 webpackConfig.mode = 'production';
 webpackConfig.devtool = 'source-map';
+webpackConfig.plugins = [
+  new MiniCssExtractPlugin({ 
+    filename: 'styles-[contenthash].css',
+  }),
+];
 webpackConfig.module.rules.push({
   test: /\.s?css$/,
   include: /frontend/,
-  loader: extractCss.extract({
-    fallback: 'style-loader',
-    use: [
-      'css-loader?modules&importLoaders=1&camelCase&localIdentName=[folder]__[local]__[hash:base64:5]',
-      'postcss-loader',
-      'sass-loader',
-    ],
-  }),
+  use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader?modules&importLoaders=1&camelCase&localIdentName=[folder]__[local]__[hash:base64:5]',
+    'postcss-loader',
+    'sass-loader',
+  ],
 });
 
 module.exports = webpackConfig;
