a6b8201f234b71b601d9441674ba83f55a2cb84b
Andree Wille <AndreeWille@users.noreply.github.com>
2021-08-13 13:23:58 +0200

    fix: MiniCssExtractPlugin configuration
    constructor takes an object instead of string

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,24 +1,24 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const webpackConfig = require('./webpack.config.base').config;
+const webpackConfig = require("./webpack.config.base").config;
 
-const extractCss = new MiniCssExtractPlugin('styles-[contenthash].css');
+const extractCss = new MiniCssExtractPlugin({
+  filename: "styles-[contenthash].css",
+});
 
-webpackConfig.mode = 'production'
-webpackConfig.devtool = 'source-map';
+webpackConfig.mode = "production";
+webpackConfig.devtool = "source-map";
 webpackConfig.module.rules.push({
   test: /\.s?css$/,
   include: /frontend/,
   use: [
     MiniCssExtractPlugin.loader,
-    'css-loader?modules&importLoaders=1&camelCase&localIdentName=[folder]__[local]__[hash:base64:5]',
-    'postcss-loader',
-    'sass-loader',
+    "css-loader?modules&importLoaders=1&camelCase&localIdentName=[folder]__[local]__[hash:base64:5]",
+    "postcss-loader",
+    "sass-loader",
   ],
 });
-webpackConfig.plugins.push(
-  extractCss
-);
+webpackConfig.plugins.push(extractCss);
 
 module.exports = webpackConfig;
