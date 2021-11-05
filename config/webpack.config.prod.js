fd6dfc2b8fe2bf7d9b2b2b510c128f79373ca370
Andree Wille <AndreeWille@users.noreply.github.com>
2021-11-05 21:15:03 +0100

    build: fix failing deployment with production env

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,26 +1,29 @@
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpackConfig = require("./webpack.config.base").config;
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const extractCss = new ExtractTextPlugin("styles-[contenthash].css");
+const extractCss = new MiniCssExtractPlugin({
+  filename: "[name]-[contenthash].css",
+  chunkFilename: "[id]-[contenthash].css",
+});
 
 webpackConfig.mode = "production";
-webpackConfig.devtool = "source-map";
 webpackConfig.module.rules.push({
   test: /\.s?css$/,
   include: /frontend/,
-  loader: extractCss.extract({
-    fallback: "style-loader",
-    use: [
-      "css-loader?modules&importLoaders=1&camelCase&localIdentName=[folder]__[local]__[hash:base64:5]",
-      "postcss-loader",
-      "sass-loader",
-    ],
-  }),
+  use: [
+    MiniCssExtractPlugin.loader,
+    "css-loader?modules&importLoaders=1&camelCase&localIdentName=[folder]__[local]__[hash:base64:5]",
+    "postcss-loader",
+    "sass-loader",
+  ],
 });
-webpackConfig.plugins.push(
-  extractCss,
-  new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
-);
+webpackConfig.plugins.push(extractCss);
+webpackConfig.optimization.minimize = true;
+webpackConfig.optimization.minimizer = [
+  new TerserPlugin(),
+  new CssMinimizerPlugin(),
+];
 
 module.exports = webpackConfig;
