493d3ebe5996e6d69e822fef773ad6c552fdfa29
Andree Wille <AndreeWille@users.noreply.github.com>
2024-08-19 08:18:59 +0200

    feat: redirect from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :redirect_to_fobizz
   after_action :set_xsrf_token_cookie
 
   def set_xsrf_token_cookie
     cookies['XSRF-TOKEN'] = form_authenticity_token
+  end
+
+  def redirect_to_fobizz
+    redirect_to(fetch_from_env('TEACH_URL'), status: 301)
   end
 end
